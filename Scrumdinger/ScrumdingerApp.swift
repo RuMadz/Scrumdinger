be11e3b9a80bc1e07945cfecc0daccedba51b52b
RuMadz <104559646+RuMadz@users.noreply.github.com>
2023-09-03 18:13:44 +0200

    Initial Commit

---
+++ Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by Mx R.G.MT on 2023/09/03.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
