3b3d4be501493dd38954dd3f680e95b28dde2902
RuMadz <104559646+RuMadz@users.noreply.github.com>
2023-09-05 05:01:54 +0200

    Second commit: I have completed the Navigation and modal presentation section

--- Scrumdinger/Models/DailyScrum.swift
+++ Scrumdinger/Models/DailyScrum.swift
@@ -7,27 +7,69 @@
 
 import Foundation
 
-struct DailyScrum {
+struct DailyScrum: Identifiable {
+    let id: UUID
     var title: String
-    var attendees: [String]
+    var attendees: [Attendee]
     var lengthInMinutes: Int
+    var lengthInMinutesAsDouble: Double {
+        get {
+            Double(lengthInMinutes)
+        }
+        set {
+            lengthInMinutes = Int(newValue)
+        }
+    }
     var theme: Theme
+    
+    init(id: UUID=UUID(), title: String, attendees: [String], lengthInMinutes: Int, theme: Theme) {
+        self.id = id
+        self.title = title
+        self.attendees = attendees.map { Attendee(name: $0) }
+        self.lengthInMinutes = lengthInMinutes
+        
+        self.theme = theme
+    }
+}
+
+extension DailyScrum {
+    struct Attendee: Identifiable{
+        let id: UUID
+        var name: String
+        
+        init(id: UUID=UUID(), name: String) {
+            self.id = id
+            self.name = name
+        }
+    }
+    static var emptyScrum: DailyScrum {
+        
+            DailyScrum ( title: "", attendees: [], lengthInMinutes: 10, theme: .sky)
+        
+    }
 }
 
 extension DailyScrum {
     static let sampleData: [DailyScrum] =
     [
-        DailyScrum(title: "Stratgey",
-                   attendees: ["Tumi", "Ru", "Tiri"],
-                   lengthInMinutes: 30,
-                   theme: .oxblood),
-        DailyScrum(title: "Budget",
-                   attendees: ["Tumi", "Ru", "Tsungi"],
-                   lengthInMinutes: 60,
-                   theme: .teal),
+        DailyScrum(
+            
+            title: "Stratgey",
+            attendees: ["Tiri","Momo","Tumi", "Ru"],
+            lengthInMinutes: 30,
+            theme: .oxblood),
+        DailyScrum( title: "Budget",
+                    attendees: ["Tumi", "Ru", "Tsungi"],
+                    lengthInMinutes: 60,
+                    theme: .teal),
         DailyScrum(title: "Sales",
                    attendees: ["Matthew", "Mark", "Luke"],
                    lengthInMinutes: 20,
                    theme: .lavender)
     ]
+    
+
+    
 }
+
+
---
+++ Scrumdinger/ScrumsView.swift
@@ -0,0 +1,39 @@
+//
+//  ScrumsView.swift
+//  Scrumdinger
+//
+//  Created by Mx R.G.MT on 2023/09/04.
+//
+
+import SwiftUI
+
+struct ScrumsView: View {
+    let scrums: [DailyScrum]
+    
+    var body: some View {
+        NavigationStack{
+            List(scrums) { scrum in
+                NavigationLink(destination: DetailView(scrum: scrum))
+                {
+                    CardView(scrum: scrum)
+                }
+                .listRowBackground(scrum.theme.mainColor)
+            }
+            .navigationTitle("Daily Scrums")
+            .toolbar {
+                Button(action: {}) {
+                    Image(systemName: "plus")
+                }
+                .accessibilityLabel("New Scrum")
+            }
+            .padding(.all, 20)
+        }
+    }
+        
+}
+
+struct ScrumsView_Previews: PreviewProvider {
+    static var previews: some View {
+        ScrumsView(scrums: DailyScrum.sampleData)
+    }
+}
